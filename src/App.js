4621f47815b23cdcc15d777e15d302c5858c94b7
stefan <stefan@stefan.com>
2023-01-19 15:43:02 +0100

    socket.io implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import socketIOClient from "socket.io-client";
+import { useState } from "react";
 
 function App() {
+  const socket = socketIOClient("http://localhost:3001");
+  const [connected, setConnected] = useState(socket);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="div_container"></div>
     </div>
   );
 }

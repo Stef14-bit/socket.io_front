9b7a8835226786510f549404226be85ac11cab1b
stefan <stefan@stefan.com>
2023-01-20 01:27:55 +0100

    conection established

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import socketIOClient from "socket.io-client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const socket = socketIOClient("http://localhost:3001");
-  const [connected, setConnected] = useState(socket);
+  const [connected, setConnected] = useState(null);
+  const [messages, setMessages] = useState([""]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("initialMessageList", messages, socket.id);
+      return () => {
+        socket.emit("disconnect", () => {
+          console.log("disconnected");
+        });
+      };
+    });
+  }, []);
 
   return (
     <div className="App">
